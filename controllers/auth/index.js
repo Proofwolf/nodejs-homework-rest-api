ea44405ae036e308ad2aee3dc153ab13953be5c5
Proofwolf <65850243+Proofwolf@users.noreply.github.com>
2024-02-09 20:10:31 +0100

    fix index.js

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -5,6 +5,7 @@ module.exports = {
   verifyEmail: ctrlWrapper(require("./verifyEmail")),
   resendVerifyEmail: ctrlWrapper(require("./resendVerifyEmail")),
   login: ctrlWrapper(require("./login")),
+  getCurrentUser: ctrlWrapper(require("./getCurrentUser")),
   logout: ctrlWrapper(require("./logout")),
   updateSubscription: ctrlWrapper(require("./updateSubscription")),
   updateAvatar: ctrlWrapper(require("./updateAvatar")),
